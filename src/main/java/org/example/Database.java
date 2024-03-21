492a16430770c0d5362980505c50e486266853ef
yi <yungpxxp@gmail.com>
2024-03-21 17:43:24 +0600

    Commit

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -1,2 +1,30 @@
-package org.example;public class Database {
+package org.example;
+import java.sql.*;
+
+public class Database {
+
+
+    static String Database_url = "jdbc:postgresql://localhost:5432/taskdb";
+    static String Login = "postgres";
+    static String Pass = "13012005aA";
+    public static int wordCountDb(String text) {
+        return text.split("\\s+").length;
+    }
+    public static void main(String[] args){
+        try(Connection connection = DriverManager.getConnection(Database_url, Login, Pass);
+            Statement statement = connection.createStatement()){
+            String sql = "select dbtext from task1_table where length(dbtext) - length(replace(dbtext, ' ', '')) + 1 >= 5;" ;
+            try (ResultSet resultSet = statement.executeQuery(sql)){
+                while (resultSet.next()){
+                    String full_text = resultSet.getString("dbtext");
+                    int word_count = wordCountDb(full_text);
+                    System.out.println("Text: " + full_text);
+                    System.out.println("Word Count: " + word_count);
+                }
+            }
+        } catch (SQLException exception){
+            exception.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/org/example/TestCounter.java
+++ src/main/java/org/example/TestCounter.java
@@ -1,2 +1,13 @@
-package org.example;public class TestCounter {
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestCounter {
+    @Test
+    public void wordCounterTest() {
+        String text = "Hello from Kazakhstan";
+        int expRes = 3;
+        assertEquals(expRes, Database.wordCountDb(text));
+    }
 }
